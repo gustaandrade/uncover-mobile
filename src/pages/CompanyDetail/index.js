6ff930bd60e45d5674894717984db391452eccae
Rafael Ferreira <andersonrafaelferreira@gmail.com>
2019-11-27 11:52:51 -0300

    company detailed

--- src/pages/CompanyDetail/index.js
+++ src/pages/CompanyDetail/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Image, View} from 'react-native';
+
+import {
+  Avatar,
+  Container,
+  Detailed,
+  CompanyMap,
+  CompanyDescription,
+  Available,
+  AvailableContainer,
+} from './styles';
 
 import Wrapper from '../../components/Wrapper';
+
+import Button from '../../components/Button';
 
 export default function CompanyDetail({navigation}) {
   const {company} = navigation.state.params;
   return (
     <Wrapper title={company.name} back={() => navigation.goBack()}>
-      <Text>Company Detail</Text>
+      <Container>
+        <Avatar source={{uri: company.image}} />
+        <Detailed>
+          <Text>{company.date}</Text>
+          <AvailableContainer>
+            <Available>{company.subscribed}</Available>
+          </AvailableContainer>
+        </Detailed>
+        <CompanyDescription>{company.description}</CompanyDescription>
+        <CompanyMap>{company.address}</CompanyMap>
+        <Button text="Quero visitar" onPress={() => {}} />
+      </Container>
     </Wrapper>
   );
 }
--- src/pages/Visits/index.js
+++ src/pages/Visits/index.js
@@ -15,13 +15,18 @@ export default function Visitis({navigation}) {
       description: 'The purple bank',
       image: 'https://media.glassdoor.com/l/827975/nubank-brasil-office.jpg',
       date: '27/11/2019',
+      address: 'Avenida São João, 677',
+      subscribed: 'INSCRITO',
     },
     {
       id: 2,
       name: 'Coca-Cola',
       description: 'The refreshing drink',
-      image: 'https://media.glassdoor.com/l/827975/nubank-brasil-office.jpg',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS1lgyHeE3hLZ9BDgOD8MJFYJbNdDm_-0XcZm38gFpF20NB6qXC',
       date: '27/11/2019',
+      address: 'Avenida Brasil, 555',
+      subscribed: 'DISPONÍVEL',
     },
   ];
 
